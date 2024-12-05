36c47a6309124f66a452ef6d815ab76be2071481
Nazarova Gulaiym <gulaiym.nazarova@alatoo.edu.kg>
2024-12-05 15:20:29 +0600

    Getting inputs from user

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
         Random random = new Random();
+        Scanner sc = new Scanner(System.in);
         int[] boxPlace = places(random);
+        int[] guessingBoxes = new int[3];
+
+            for (int i = 0; i < 3; i++) {
+                //System.out.print("Enter kilometer " + (i + 1) + ": ");
+                guessingBoxes[i] = sc.nextInt();
+
+            }
     }
     public static int[] places(Random random)
     {
